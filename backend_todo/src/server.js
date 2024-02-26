113d16f87d5c154a323e349bebdf5c7b430ce366
Nexus-coder <68616882+Nexus-coder@users.noreply.github.com>
2024-02-26 13:39:57 +0300

    Changed the database to atlas mongodb

--- backend_todo/src/server.js
+++ backend_todo/src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 4000;
 const server = http.createServer(app);
 
 // const mongoURI = `mongodb://127.0.0.1:27017/Todo`;
-const mongoURI = `"mongodb+srv://andrew:51q0WX5Jx89ickU3@cluster0.hbs3wtk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"`;
+const mongoURI = `mongodb+srv://andrew:51q0WX5Jx89ickU3@cluster0.hbs3wtk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 mongoose.connection.once("on", () => {
   console.log("MongoDB server coonection opened");
